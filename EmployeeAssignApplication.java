10082b43e9123fd10bf8731e8bd78784db96b8b2
21dineshk <42747190+21dineshk@users.noreply.github.com>
2020-05-25 12:47:07 +0530

    Add files via upload

---
+++ EmployeeAssignApplication.java
@@ -0,0 +1,14 @@
+package com.ensemble.employeeAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssignApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
